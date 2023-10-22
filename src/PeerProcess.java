d9650bf6b9f1ce8058d35a4e2a98ff21d76e7ce1
ChristianBell002 <Christian.Bello@ufl.edu>
2023-10-21 23:45:59 -0400

    Started Peer Process, testing commit.

--- src/PeerProcess.java
+++ src/PeerProcess.java
@@ -1,3 +1,19 @@
-public class PeerProcess {
+import java.net.*;
+import java.io.*;
+import java.nio.*;
+import java.nio.channels.*;
+import java.util.*;
+
+public class PeerProcess extends Thread { // each peer is both a client and a server. Reads config file and creates peer object. 
+    private int ID;
+    private int port;
+    private String host;
+
+    public Peer(int ID, int port, String host){ 
+        this.id = id;
+        this.port = port;
+        this.host = host;
+    }
+
     
 }
--- src/Threads/Client.java
+++ src/Threads/Client.java
@@ -1,5 +1,63 @@
-package Threads;
+//package Threads;
+import java.net.*;
+import java.io.*;
+import java.nio.*;
+import java.nio.channels.*;
+import java.util.*;
 
-public class Client {
-    
+public class Client extends Thread {
+    Socket requestSocket;
+    ObjectOutputStream out;
+    ObjectInputStream in;
+    String message;
+    String MESSAGE;
+
+    void run(){
+        try{
+            requestSocket = new Socket("localhost", 8000);
+            System.out.println("Connected to localhost in port" + requestSocket.port);
+            //initialize input and output stream
+            out = new ObjectOutputStream(requestSocket.getOutputStream());
+            out.flush(); // clears the output stream. 
+            in = new ObjectInputStream(requestSocket.getInputStream());
+
+            BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+
+        }
+
+        catch (ConnectException e){
+            System.err.println("Connection refused. You need to initiate a server first.");
+        }
+        catch (ClassNotFoundException e){
+            System.err.println("Class not found.");
+        }
+        catch(UnknownHostException unknownHost){
+            System.err.println("You are trying to connect to an unknown host!"); 
+        }
+        finally{ // Close connection
+
+        try{
+            in.close();
+            out.close();
+            requestSocket.close();
+        }
+        catch(IOException ioException){
+            ioException.printStackTrace();
+        }
+    }
 }
+
+    void sendMessage(String message){
+        try{
+            out.writeObject(message);
+            out.flush();
+        }
+        catch(IOException ioException){
+            ioException.printStackTrace();
+        }
+    }
+
+    public static void main(String args[]){
+        Client client = new Client();
+        client.run();
+    }
