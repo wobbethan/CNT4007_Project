babae9936f7fce4ff59014f523be204bc0f22d44
Ethan Wobb <92873738+wobbethan@users.noreply.github.com>
2023-10-22 14:07:20 -0400

    CommonParser working/tested

--- src/CommonParser.java
+++ src/CommonParser.java
@@ -29,21 +29,26 @@ public class CommonParser {
         return pieceSize;
     }
 
+
     //May need setter functions ;)
     
     public void read(){
         Properties commonCfg = new Properties();
-        
-        
-        try (FileInputStream fileInput = new FileInputStream("/Canvas/Project/project_config_file_small/project_config_file_small/Common.cfg")) { //maybe wrong file path
-                commonCfg.load(fileInput);
 
-                numPreferredNeighbors = Integer.parseInt(commonCfg.getProperty("NumberOfPreferredNeighbors")); //extract preferred Neighbors and assign
-                unchokingInterval = Integer.parseInt(commonCfg.getProperty("UnchokingInterval"));
-                optimisticUnchokingInterval = Integer.parseInt(commonCfg.getProperty("OptimisticUnchokingInterval"));
-                fileName = commonCfg.getProperty("FileName");
-                fileSize = Integer.parseInt(commonCfg.getProperty("FileSize"));
-                pieceSize = Integer.parseInt(commonCfg.getProperty("PieceSize"));
+
+        try(FileInputStream fileInput = new FileInputStream("Canvas\\Project\\project_config_file_small\\project_config_file_small\\Common.cfg")) { //maybe wrong file path
+
+            commonCfg.load(fileInput);
+            this.numPreferredNeighbors = Integer.parseInt(commonCfg.getProperty("NumberOfPreferredNeighbors")); //extract preferred Neighbors and assign
+            this.unchokingInterval = Integer.parseInt(commonCfg.getProperty("UnchokingInterval"));
+            this.optimisticUnchokingInterval = Integer.parseInt(commonCfg.getProperty("OptimisticUnchokingInterval"));
+            this.fileName = commonCfg.getProperty("FileName");
+            this.fileSize = Integer.parseInt(commonCfg.getProperty("FileSize"));
+            this.pieceSize = Integer.parseInt(commonCfg.getProperty("PieceSize"));
+
+
+        } catch (FileNotFoundException ex) {
+            System.out.println("File not found");
         } catch (IOException e) {
             e.printStackTrace();
         }
@@ -53,14 +58,21 @@ public class CommonParser {
         System.out.println("-----------------------------------------");
         System.out.println("Testing file parsing:     ");
         System.out.println("Number of preferred neighbors: " + numPreferredNeighbors);
-        System.out.println("Unchoking Interval: " + unchokingInterval);
+        System.out.println("UnChoking Interval: " + unchokingInterval);
         System.out.println("Optimistic Interval: " + optimisticUnchokingInterval);
-        System.out.println("Filename: " + fileName);
+        System.out.println("Filename: " + this.fileName);
         System.out.println("File Size: " + fileSize);
         System.out.println("Piece Size: " + pieceSize);
         System.out.println("-----------------------------------------");
 
     }
+
+    //public static void main(String[] args){ //Internal Main for testing
+    //    CommonParser testParser = new CommonParser();
+    //    testParser.read();
+    //    testParser.testParser();
+    //
+    //}
     
     
 }
