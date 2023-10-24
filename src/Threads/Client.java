4700a92c50ce792840f0a636b3f1c37d8b926e35
Ryan McPherson <ryan7mcpherson@gmail.com>
2023-10-24 17:16:03 -0400

    chore: add basic handshake sending and new local testing config

--- src/Threads/Client.java
+++ src/Threads/Client.java
@@ -22,26 +22,94 @@ public class Client extends Thread {
     }
 
     public void run() {
-        
-            
         // try to connect to every peer in the network
         for (Integer id : neighboringPeers.keySet()) {
             try {
-                // FIXME: neighboringPeers is blank for some reason, perhaps not getting
-                // assigne correctly in PeerProcess
-                System.out.println(neighboringPeers.get(id)[0] + " " +  Integer.parseInt(neighboringPeers.get(id)[1]));
+                Socket socket = new Socket(neighboringPeers.get(id)[0], Integer.parseInt(neighboringPeers.get(id)[1]));    
 
-                Socket serverSocket = new Socket(neighboringPeers.get(id)[0], Integer.parseInt(neighboringPeers.get(id)[1]));    
-                
-                // send handshake
-                Handshake handshake = new Handshake(peerID);
-                ObjectOutputStream out = new ObjectOutputStream(serverSocket.getOutputStream());
-                out.writeObject(handshake);
-                out.flush();
+                // send server handshake
+                Handshake clientHandshake = new Handshake(peerID);
+                sendServerHandshake(socket, clientHandshake.getHandshakeAsByteArray());
+
+                // receive server handshake
+                byte[] serverHandshake = receiveServerHandshake(socket);
+                System.out.println("client thread: " + new String (serverHandshake, "US-ASCII"));
+
             } catch (IOException e) {
                 System.err.println(e);
             }
         }
     }
+
+    /**
+	 * retrieves the handshake message sent from server peer to client peer socket
+	 * 
+	 * @param socket communication socket between client peer and server peer
+	 * @return handshake message as a byte[]
+	 */
+	private byte[] receiveServerHandshake(Socket socket) {
+		byte[] handshake = null;
+		try {
+			ObjectInputStream inStream = new ObjectInputStream(socket.getInputStream());
+			handshake = (byte[]) inStream.readObject();
+		} catch (IOException e) {
+			System.err.println(e);
+		} catch (ClassNotFoundException e) {
+			System.err.println(e);
+		}
+
+		return handshake;
+	}
+
+	/**
+	 * sends the server peer a handshake message from the client peer over the
+	 * socket
+	 * 
+	 * @param socket    communication socket between client peer and server peer
+	 * @param handshake handshake message as a byte[]
+	 */
+	private void sendServerHandshake(Socket socket, byte[] handshake) {
+		try {
+			ObjectOutputStream outStream = new ObjectOutputStream(socket.getOutputStream());
+			outStream.writeObject(handshake);
+		} catch (IOException e) {
+			System.err.println(e);
+		}
+	}
+
+	/**
+	 * retrieves the bitfield message sent from server peer to client peer socket
+	 * 
+	 * @param socket communication socket between client peer and server peer
+	 * @return bitfield message as a byte[]
+	 */
+	private byte[] receiveServerBitfield(Socket socket) {
+		byte[] bitfield = null;
+		try {
+			ObjectInputStream inStream = new ObjectInputStream(socket.getInputStream());
+			bitfield = (byte[]) inStream.readObject();
+		} catch (IOException e) {
+			System.err.println(e);
+		} catch (ClassNotFoundException e) {
+			System.err.println(e);
+		}
+
+		return bitfield;
+	}
+
+	/**
+	 * sends the server peer a bitfield message from the client peer over the socket
+	 * 
+	 * @param socket   communication socket between client peer and server peer
+	 * @param bitfield the server's bitfield as a byte[]
+	 */
+	private void sendServerBitfield(Socket socket, byte[] bitfield) {
+		try {
+			ObjectOutputStream outStream = new ObjectOutputStream(socket.getOutputStream());
+			outStream.writeObject(bitfield);
+		} catch (IOException e) {
+			System.err.println(e);
+		}
+	}
 
 }
--- src/Threads/Server.java
+++ src/Threads/Server.java
@@ -6,6 +6,8 @@ import java.nio.*;
 import java.nio.channels.*;
 import java.util.*;
 
+import Messages.Handshake;
+
 public class Server extends Thread {
 	private int portNum;
 	private int peerID;
@@ -21,16 +23,22 @@ public class Server extends Thread {
 			ServerSocket listenerSocket = new ServerSocket(portNum);
 
 			while (true) {
-				Socket clientSocket = listenerSocket.accept();
+				Socket socket = listenerSocket.accept();
 
-				System.out.println("client connected!");
+				// receive client handshake
+				byte[] clientHandshake = receiveClientHandshake(socket);
+                System.out.println("server thread: " + new String (clientHandshake, "US-ASCII"));
+
+				// send client handshake
+				Handshake serverHandshake = new Handshake(peerID);
+				sendClientHandshake(socket, serverHandshake.getHandshakeAsByteArray());
 
 				// TODO: receive client handshake
 
 				// TODO: send handshake
 
 				// TODO: check client handshake valid
-				
+
 				// TODO: check client id in handshake is contained within config file
 
 				// TODO: send server bitfield to client
@@ -39,12 +47,12 @@ public class Server extends Thread {
 
 				// TODO: log tcp connection established
 
-				// TODO: maybe find some way to track all peers that have a file				
-				
+				// TODO: maybe find some way to track all peers that have a file
+
 				// TODO: spawn send message thread
-				
+
 				// TODO: spawn request piece thread
-				
+
 				// TODO: spawn receive message thread
 
 			}
@@ -53,4 +61,76 @@ public class Server extends Thread {
 		}
 
 	}
+
+	/**
+	 * retrieves the handshake message sent from client peer to server peer socket
+	 * 
+	 * @param socket communication socket between client peer and server peer
+	 * @return handshake message as a byte[]
+	 */
+	private byte[] receiveClientHandshake(Socket socket) {
+		byte[] handshake = null;
+		try {
+			ObjectInputStream inStream = new ObjectInputStream(socket.getInputStream());
+			handshake = (byte[]) inStream.readObject();
+		} catch (IOException e) {
+			System.err.println(e);
+		} catch (ClassNotFoundException e) {
+			System.err.println(e);
+		}
+
+		return handshake;
+	}
+
+	/**
+	 * sends the client peer a handshake message from the server peer over the
+	 * socket
+	 * 
+	 * @param socket    communication socket between client peer and server peer
+	 * @param handshake handshake message as a byte[]
+	 */
+	private void sendClientHandshake(Socket socket, byte[] handshake) {
+		try {
+			ObjectOutputStream outStream = new ObjectOutputStream(socket.getOutputStream());
+			outStream.writeObject(handshake);
+		} catch (IOException e) {
+			System.err.println(e);
+		}
+	}
+
+	/**
+	 * retrieves the bitfield message sent from client peer to server peer socket
+	 * 
+	 * @param socket communication socket between client peer and server peer
+	 * @return bitfield message as a byte[]
+	 */
+	private byte[] receiveClientBitfield(Socket socket) {
+		byte[] bitfield = null;
+		try {
+			ObjectInputStream inStream = new ObjectInputStream(socket.getInputStream());
+			bitfield = (byte[]) inStream.readObject();
+		} catch (IOException e) {
+			System.err.println(e);
+		} catch (ClassNotFoundException e) {
+			System.err.println(e);
+		}
+
+		return bitfield;
+	}
+
+	/**
+	 * sends the client peer a bitfield message from the server peer over the socket
+	 * 
+	 * @param socket   communication socket between client peer and server peer
+	 * @param bitfield the server's bitfield as a byte[]
+	 */
+	private void sendClientBitfield(Socket socket, byte[] bitfield) {
+		try {
+			ObjectOutputStream outStream = new ObjectOutputStream(socket.getOutputStream());
+			outStream.writeObject(bitfield);
+		} catch (IOException e) {
+			System.err.println(e);
+		}
+	}
+
 }
