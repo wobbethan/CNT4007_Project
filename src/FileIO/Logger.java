d4293d43e8c74cdaeff1996b3cae141e130e86c1
Ryan McPherson <ryan7mcpherson@gmail.com>
2023-12-01 16:54:05 -0500

    chore: light refactor and add preferred and optimistically chosen neighbors

--- src/FileIO/Logger.java
+++ src/FileIO/Logger.java
@@ -30,15 +30,45 @@ public class Logger {
         return currentTime.format(formatter);
     }
 
-    public void logTCPConnection(int connectingPeerId) {
+    private void logMessage(String message) {
         try {
             BufferedWriter writer = new BufferedWriter(new FileWriter(fileName, true));
-            writer.write("[" + fetchCurrentTime() + "]: Peer " + peerId + " makes a connection to Peer "
-                    + connectingPeerId + "\n");
+            writer.write(message);
             writer.close();
         } catch (IOException e) {
             e.printStackTrace();
         }
+    }
+
+    public void logTCPConnection(int connectingPeerId) {
+        String message = "[" + fetchCurrentTime() + "]: Peer " + peerId + " makes a connection to Peer "
+                + connectingPeerId + ".\n";
+
+        logMessage(message);
+    }
+
+    public void logChangePreferredNeighbors(int[] preferredNeighbors) {
+        if (preferredNeighbors == null || preferredNeighbors.length == 0) {
+            System.err.println("ERROR: preferred neighbors list is invalid");
+            return;
+        }
+
+        String csv = preferredNeighbors[0] + "";
+        for (int i = 1; i < preferredNeighbors.length; i++) {
+            csv += "," + preferredNeighbors[i];
+        }
+
+        String message = "[" + fetchCurrentTime() + "]: Peer " + peerId + " has the preferred neighbors " +
+                csv + ".\n";
+
+        logMessage(message);
+    }
+
+    public void logChangeOptimisticallyUnchokedNeighbor(int neighbor) {
+        String message = "[" + fetchCurrentTime() + "]: Peer " + peerId + " has the optimistically unchoked neighbor "
+                + neighbor + ".\n";
+
+        logMessage(message);
     }
 
 }
